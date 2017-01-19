f878f34c4278156208ba7a65c6d94a73e1a3f082
samer <samaladhami@gmail.com>
2017-01-18 20:37:17 -0600

    added invalid number handler

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
 
-  
 
+  function removeInvalidNumberMode(){
+    $('.number-input').removeClass('invalid-input');
+    $('#invalid-input-text').css('visibility', ' hidden')
+  }
+  $('.number-input').click( () => {
+    removeInvalidNumberMode()
+  })
+
+  $('#count-button-id').click( () => {
+    const inputVal = Number( $('.number-input').val() )
+    // check the input value
+    if (isNaN(inputVal)){
+      //invalid number
+      $('.number-input').addClass('invalid-input');
+      $('#invalid-input-text').css('visibility', ' visible');
+    }
+    else{
+      // valid number
+      
+
+    }
+  })
 })
