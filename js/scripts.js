1f48cadec3ee3b5e00b84e0ba87f019b2c2494c6
samer <samaladhami@gmail.com>
2017-01-18 23:25:19 -0600

    finished script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,84 @@
+"use strict"
+
 $(document).ready(function(){
 
-  let counter = 0;
+  let counter = 0
   function removeInvalidNumberMode(){
-    $('.number-input').removeClass('invalid-input');
-    $('#invalid-input-text').css('visibility', ' hidden')
+    $( '.number-input' ).removeClass( 'invalid-input' );
+    $( '#invalid-input-text' ).css( 'visibility', ' hidden' )
   }
   // removes invalid number mode when the user clicks on the input field
-  $('.number-input').click( () => {
+  $( '.number-input' ).click( () => {
     removeInvalidNumberMode()
   })
 
-  $('#count-button-id').click( () => {
-    const inputVal = $('.number-input').val();
+  $( '#count-button-id' ).click( () => {
+    const inputVal = $( '.number-input' ).val();
     // check the input value
-    if ( Number( isNaN(inputVal) ) || inputVal === ''){
+    if ( Number( isNaN( inputVal ) ) || inputVal === ''){
       //invalid number handler
-      $('.number-input').addClass('invalid-input');
-      $('#invalid-input-text').css('visibility', ' visible');
+      $( '.number-input' ).addClass( 'invalid-input' );
+      $( '#invalid-input-text' ).css( 'visibility', ' visible' );
     }
     else{
       // valid number
-      $('#count-button-id, .number-input').prop('disabled',true)
+      $( '#count-button-id, .number-input' ).prop( 'disabled',true )
                                           .css({
                                                 'opacity': '0.8' ,
                                                 'cursor' : 'not-allowed'
-                                              });
-      for (let i = 1; i <= inputVal ; i++) {
-        setTimeout(function () {
-          $('.counter > h2').text(i);
-        }, i * 1000)
+                                            });
+      // checks a number if it's divisible by 5, 3 or both
+      function checkTheCounter(num){
+        if( num % 3 === 0 && num % 5 === 0) {
+          console.log(num % 3 , num % 5 );
+          $( '#fingers' ).css( 'background-color', '#ffcb7d' );
+          $('#toes').css( 'background-color', '#ff6fb8' );
+        }
+        else if( num % 3 === 0){
+          console.log(num % 3);
+          $( '#fingers' ).css( 'background-color', '#ffcb7d' );
+        }
+        else if( num % 5 === 0) {
+          console.log(num % 5);
+          $( '#toes' ).css( 'background-color', '#ff6fb8' );
+        }
       }
+      //
+      // this variable outside startCount function so I can have access to it.
+      let countInterval;
+      function startCount() {
+        // countInterval = intervalID so I can use clearInterval(intervalID)
+        countInterval = setInterval( ( ) => {
+          if( counter === Number(inputVal) ) {
+            clearInterval( countInterval );
+          }
+          else {
+            counter++;
+            $( '#fingers, #toes' ).removeAttr('style');
+            $( '.counter > h2' ).text( counter );
+            checkTheCounter(counter);
+          }
+        } , 1000 );
+      }
+      startCount();
 
+      $( '#reset' ).click( () => {
+        $( '#count-button-id, .number-input' ).removeAttr('style')
+                                              .prop( 'disabled',false );
+        clearInterval( countInterval );
+        counter = 0;
+        $( '.counter > h2' ).text( counter );
+        $( '.number-input' ).val('');
+        $( '#fingers, #toes' ).removeAttr('style');
+      });
 
+      $('#restart').click( () => {
+        clearInterval( countInterval );
+        startCount();
+        counter = 0;
+        $( '#fingers, #toes' ).removeAttr('style');
+        $( '.counter > h2' ).text( counter );
+      })
     }
   })
 })
