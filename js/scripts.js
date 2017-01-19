6e2e7e9d13b738022bb30118708c823b1bc952c9
samer <samaladhami@gmail.com>
2017-01-18 21:02:09 -0600

    disabled the input field and the count button  when the enters valid number

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,18 @@ $(document).ready(function(){
     const inputVal = Number( $('.number-input').val() )
     // check the input value
     if (isNaN(inputVal)){
-      //invalid number
+      //invalid number handler
       $('.number-input').addClass('invalid-input');
       $('#invalid-input-text').css('visibility', ' visible');
     }
     else{
       // valid number
-      
+      $('#count-button-id, .number-input').prop('disabled',true)
+                                          .css({
+                                                'opacity': '0.8' ,
+                                                'cursor' : 'not-allowed'
+                                              });
+
 
     }
   })
